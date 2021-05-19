51aad572eca431687c686447555f0852bb42bdcc
Abhishek Raj <54662059+iamraj45@users.noreply.github.com>
2021-05-19 16:27:10 +0530

    Update script.py

--- link-auto-sender/script.py
+++ link-auto-sender/script.py
@@ -1,7 +1,7 @@
 from twilio.rest import Client
 
-account_sid = 'ACabee3e05e938ad82c003b21e5eb11e08'
-auth_token = 'd856918f9f3bc813b4a66d0cb45d9281'
+account_sid = 'put your account sid from twilio here'
+auth_token = 'put the auth_token from twilio here'
 client = Client(account_sid, auth_token)
 
 linkTAFL = "https://zoom.us/j/92782378685?pwd=VTVKMnJ3aC9lR3diOG9PeEJxd2VWdz09"
@@ -13,7 +13,7 @@ def send():
     message = client.messages.create(
         from_='whatsapp:+14155238886',
         body="The link for your TAFL class is "+ linkTAFL,
-        to='whatsapp:+917413936216'
+        to='whatsapp:+91**********'
     )
     print(message.sid)
 
@@ -22,7 +22,7 @@ def send1():
     message = client.messages.create(
         from_='whatsapp:+14155238886',
         body="The link for your Maths class is "+ linkMaths,
-        to='whatsapp:+917413936216'
+        to='whatsapp:+91**********'
     )
     print(message.sid)
 
@@ -31,7 +31,7 @@ def send2():
     message = client.messages.create(
         from_='whatsapp:+14155238886',
         body="The link for your Microprocessor class is "+ linkMP,
-        to='whatsapp:+917413936216'
+        to='whatsapp:+91**********'
     )
     print(message.sid)
 
@@ -40,6 +40,6 @@ def send3():
     message = client.messages.create(
         from_='whatsapp:+14155238886',
         body="The link for your Mentoring class is "+ linkMentor,
-        to='whatsapp:+917413936216'
+        to='whatsapp:+91**********'
     )
     print(message.sid)
